23adaaf7b8a2eb63b79dde1e3f3ed62efea6ba83
jayshiai <jayvardhanpatil7811@gmail.com>
2023-11-13 20:35:37 +0530

    change reciever email

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -28,7 +28,7 @@ export const POST = async (req) => {
   });
   const mailData = {
     from: "tantrafiestadummymailer@gmail.com",
-    to: "jayvardhanpatil7811@gmail.com",
+    to: "clinicparasmedical@gmail.com",
     subject: `Appointment Booking by ${data.name}`,
     text: data.type + " | Phone no: " + data.phone,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="und"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>New Template</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> <!--[if gte mso 9]><xml> <o:OfficeDocumentSettings> <o:AllowPNG></o:AllowPNG> <o:PixelsPerInch>96</o:PixelsPerInch> </o:OfficeDocumentSettings> </xml>
